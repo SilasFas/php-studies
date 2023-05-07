34e569bc68d752c2c21d35a7d960c528e3a0d117
Silas <silasfas@hotmail.com>
2023-05-07 00:58:13 -0300

    desafio 1

--- exercicios/desafios/d001/resposta.php
+++ exercicios/desafios/d001/resposta.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resposta backend</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -13,16 +14,18 @@
         <h1>Resultado Final</h1>
         <p>
             <?php
-            $numero = $_REQUEST["numero"];
+            $numero = $_REQUEST["numero"] ?? 0;
             $antecessor = $numero - 1;
             $sucessor = $numero + 1;
 
-            echo "O antecessor de $numero é . $antecessor e o $numero " 
+            echo "<p> O antecessor de  $numero é $antecessor e o sucessor de $numero é $sucessor</p>";
 
-
+            echo "O número escolhido foi: <strong>$numero</strong>";
+            echo "<br> O seu antecessor é $antecessor";
+            echo "<br> O seu sucessor é $sucessor"
             ?>
-
         </p>
+        <button onclick="javascript:history.go(-1)">&#x2B05; Voltar</button>
     </main>
 </body>
 
