a18897804e15df4c38b72307f644dac14dba449c
Silas <silasfas@hotmail.com>
2023-05-07 11:22:53 -0300

    desafio 2

---
+++ exercicios/desafios/d002/resposta.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabalahndo com números aleatórios</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <main>
+        <h1>Trabalahndo com números aleatórios</h1>
+        <p>Gerando número aleatório entre 0 e 100...</p>
+        <?php
+        $numero_aleatorio = mt_rand(1, 100);
+        echo " O valor gerado foi <strong>$numero_aleatorio</strong>";
+        ?>
+        <button onclick="javascript:document.location.reload()">&#x1F504; Gerar outro</button>
+    </main>
+</body>
+
+</html>
