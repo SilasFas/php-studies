a1b1f209d07c77daa558c6af55556f6d7e06660b
Silas <silasfas@hotmail.com>
2023-05-25 01:28:52 -0300

    arithmetic

---
+++ others/3 - arithmetic/index.php
@@ -0,0 +1,37 @@
+<?php 
+
+// Arithmetic operators
+// + -  * / ** %
+
+$x = 10;
+$y = 2;
+$z = null;
+
+$z = $x + $y;
+$z = $x - $y;
+$z = $x * $y;
+$z = $x / $y;
+$z = $x ** $y;
+$z = $x % $y;
+
+echo $z;
+echo "<br>";
+
+// Increment/decrement operators
+// ++, --
+
+$counter = 10;
+$counter++;
+$counter--;
+$counter+=5;
+$counter-=10;
+
+echo $counter;
+
+// Operator Precedence 
+// ()
+// **
+// * / %
+// + -
+
+?>
