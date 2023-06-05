1ced251530b0dd68faebdf057545ee4173267923
Silas <silasfas@hotmail.com>
2023-06-05 16:44:26 -0300

    string functions

---
+++ others/17 - string functions/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$username = "Silas Fas Male";
+$phone = "123-456-7890";
+$fruits = array("banana", "Orange", "apple");
+
+//$username = strtolower($username);
+//$username = strtoupper($username);
+//$username = trim($username);
+//$username = str_pad($username, 20, "0");
+//$phone = str_replace("-", " // ", $phone);
+//$username = strrev($username);
+//$username = str_shuffle($username);
+//$equals = strcmp($username, "Silas Fas");
+// $count = strlen($username);
+// $index = strpos($username, " ");
+// $index = strpos($username, "F");
+// $first_name = substr($username, 0, 5);
+// $last_name = substr($username, 6);
+// $fullname = explode(" ", $username);
+// $fruits = implode("--", $fruits);
+
+// // foreach ($fullname as $name) {
+// //     echo $name . "<br>";
+// // }
+
+// echo $fruits;
