6db6717396edaad8ccfd0827b9ee2ddcb77b8c30
Silas <silasfas@hotmail.com>
2023-06-07 00:30:58 -0300

    update

---
+++ others/23 - password hashing/index.php
@@ -0,0 +1,6 @@
+<?php 
+// hashing = trasnforming sensitive data (password)
+//           into letters, numbers, and/or symbols 
+//           via a mathematical process. (similar to encryption) 
+//           Hides the original data from 3rd parties.
+?>
