ae4a31cf366f7c1f78286efb16360d626a05ee54
Silas <silasfas@hotmail.com>
2023-06-09 14:04:48 -0300

    hashing

--- others/23 - password hashing/index.php
+++ others/23 - password hashing/index.php
@@ -1,6 +1,15 @@
-<?php 
+<?php
 // hashing = trasnforming sensitive data (password)
 //           into letters, numbers, and/or symbols 
 //           via a mathematical process. (similar to encryption) 
 //           Hides the original data from 3rd parties.
-?>
+
+$password = "Silas1+*";
+$hash = password_hash($password, PASSWORD_DEFAULT);
+echo $hash . "<br><br>";
+
+if (password_verify("pizza123", $hash)) {
+    echo "You are logged in";
+} else {
+    echo "Incorrect password";
+}
