c619528f18aa39e0d38d38a488c14f10fc8c8e94
Silas <silasfas@hotmail.com>
2023-06-14 01:07:43 -0300

    desafio 12

--- exercicios/desafios/d012/index.php
+++ exercicios/desafios/d012/index.php
@@ -12,33 +12,38 @@
     <main>
         <h1>Calculadora de tempo</h1>
         <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
-            <label for="number">Qual é o total de segunddos?</label>
+            <label for="number">Qual é o total de segundos?</label>
             <input type="number" name="number" id="number">
             <input type="submit" value="Calcular">
         </form>
     </main>
     <section>
         <?php
-        $number = $_GET['number'];
-        if (empty($number)) {
-            $number = 0;
-        }
+            $number = $_GET['number'] ?? 0;
+            if (empty($number)) {
+                $number = 0;
+            }
+            $semanas = $number / 604800;
+            $resto1 =  $number % 604800;
 
-        // continuar aqui
-        
+            $dias = $resto1 / 86400;
+            $resto2 = $resto1 % 86400;
 
+            $hora = $resto2 / 3600;
+            $resto3 = $resto2 % 3600;
 
+            $minutos = $resto3 / 60;
 
-
+            $segundos = $resto3 % 60;
         ?>
         <h2>Totalizando tudo</h2>
         <p>Analisando o valor que você degitou, <strong><?php echo $number ?> segundos</strong> equivalem a um total de: </p>
         <ul>
-            <li> XXX semanas</li>
-            <li> XXX dias</li>
-            <li> XXX horas</li>
-            <li> XXX minutos</li>
-            <li> XXX segundos</li>
+            <li> <?php echo (int) $semanas ?> semanas</li>
+            <li> <?php echo (int) $dias ?> dias</li>
+            <li> <?php echo (int) $hora ?> horas</li>
+            <li> <?php echo (int) $minutos ?> minuto(s)</li>
+            <li> <?php echo (int) $segundos ?> segundos</li>
         </ul>
     </section>
 </body>
